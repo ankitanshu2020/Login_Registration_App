3d212db83762a30c782c0af1f31fc69242872f6e
Ankitanshu Kumar <60036164+ankitanshu2020@users.noreply.github.com>
2024-01-28 13:34:19 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,80 @@
+package com.registration;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.protocol.Resultset;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uemail = request.getParameter("username");
+		String upwd = request.getParameter("password");
+		
+		HttpSession session = request.getSession();
+		
+		RequestDispatcher dispatcher = null;
+		
+		Connection con = null;
+		
+		if(uemail == null || uemail.equals("")) {
+			request.setAttribute("status","invalidEmail");
+			dispatcher = request.getRequestDispatcher("login.jsp");
+			dispatcher.forward(request, response);
+		}	
+		if(upwd == null || upwd.equals("")) {
+			request.setAttribute("status","invalidUpwd");
+			dispatcher = request.getRequestDispatcher("login.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");	
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/signup", "root", "root");
+			
+			PreparedStatement pst = con.prepareStatement("select * from users where uemail = ? and upwd = ?");
+			
+			pst.setString(1, uemail);
+			pst.setString(2, upwd);
+			
+			ResultSet rs = pst.executeQuery();
+			
+			if(rs.next()) {
+				session.setAttribute("name", rs.getString("uname"));
+				dispatcher = request.getRequestDispatcher("index.jsp");
+			} else {
+				request.setAttribute("status", "failed");
+				dispatcher = request.getRequestDispatcher("login.jsp");
+			}
+			
+			dispatcher.forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
